847f51b36781797208b5c540a2b75d9b32c1804f
khanhhienbi <40361758+khanhhienbi@users.noreply.github.com>
2018-08-02 13:13:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 32321;
 const bq = require('./query')
 
+
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -26,37 +27,29 @@ app.get('/getAnalyst', (req, res) => {
     })
 })
 
-app.get('/getTweets', (req, res) => {
-    bq.getTweets().then((data) => {
+app.get('/getMADeals/:count?', (req, res) => {
+    bq.getMADeals(req.params.count).then((data) => {
         res.json(data);
     }).catch((error) => {
         console.log(error);
     });
 });
 
-app.get('/customQuery/:tableName/:count', (req, res) => {
-    bq.customQuery(req.params.tableName, req.params.count).then((data) => {
+app.get('/getSummary', (req, res) => {
+    bq.getSummary().then((data) => {
         res.json(data);
     }).catch((error) => {
         console.log(error);
     });
 });
 
-app.get('/getTable', (req, res) => {
-    bq.getTable().then((data) => {
+app.get('/customQuery/:query', (req, res) => {
+    bq.customQuery(req.params.query).then((data) => {
         res.json(data);
     }).catch((error) => {
         console.log(error);
     });
 });
-
-app.get('/runTestQuery', (req, res) => {
-    bq.runTestQuery().then((data) => {
-        res.json(data);
-    }).catch((error) => {
-        console.log(error);
-    });
-})
 
 app.listen(port, () => {
   console.log(`listening on port ${ port }`);
